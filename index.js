ea59a2d546722606c318c03860a10ade8f3553fc
Ketki Kule <kule.k@northeastern.edu>
2022-04-14 11:20:42 -0400

    changed TTL to 2mins

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ aws.config.update({
 var ses = new aws.SES({
   region: "us-east-1"
 });
-var docClient = new aws.DynamoDB.DocumentClient();
+var docClient = new aws.DynamoDB.DocumentClient({
+  region: 'us-east-1'
+});
 exports.handler = async function (event) {
   let message = event.Records[0].Sns.Message
   let json = JSON.parse(message);
   let email = json.username;
   let token = json.token;
-  const seconds = 5 * 60;
+  const seconds = 2 * 60;
   const secondsInEpoch = Math.round(Date.now() / 1000);
   const expirationTime = secondsInEpoch + seconds;
   const currentTime = Math.round(Date.now() / 1000);
